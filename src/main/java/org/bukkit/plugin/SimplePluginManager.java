224a337644ad70b9ec344167d549f59850154fb1
Aikar <aikar@aikar.co>
2013-06-02 10:42:57 +1000

    Measure Timings Duration

--- src/main/java/org/bukkit/plugin/SimplePluginManager.java
+++ src/main/java/org/bukkit/plugin/SimplePluginManager.java
@@ -274,6 +274,7 @@ public final class SimplePluginManager implements PluginManager {
             }
         }
 
+        org.bukkit.command.defaults.TimingsCommand.timingStart = System.nanoTime(); // Spigot
         return result.toArray(new Plugin[result.size()]);
     }
 
