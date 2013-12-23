033a0b5dd402d237790369c93b8a6723ea30ef4a
md_5 <git@md-5.net>
2013-12-24 10:14:25 +1100

    Fix Tab Completion for Some Commands

--- src/main/java/org/bukkit/command/defaults/PluginsCommand.java
+++ src/main/java/org/bukkit/command/defaults/PluginsCommand.java
@@ -40,4 +40,12 @@ public class PluginsCommand extends BukkitCommand {
 
         return "(" + plugins.length + "): " + pluginList.toString();
     }
+
+    // Spigot Start
+    @Override
+    public java.util.List<String> tabComplete(CommandSender sender, String alias, String[] args) throws IllegalArgumentException
+    {
+        return java.util.Collections.emptyList();
+    }
+    // Spigot End
 }
