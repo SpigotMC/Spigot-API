dcd4573f8dac59c113c473876096fa45b93b5ea6
libraryaddict <redwarfare@live.com>
2014-08-22 05:31:04 -0400

    Added isUnbreakable and setUnbreakable to ItemMeta

--- src/main/java/org/bukkit/inventory/meta/ItemMeta.java
+++ src/main/java/org/bukkit/inventory/meta/ItemMeta.java
@@ -124,6 +124,34 @@ public interface ItemMeta extends Cloneable, ConfigurationSerializable {
     */
     boolean hasConflictingEnchant(Enchantment ench);
 
+    // Spigot start
+    public class Spigot
+    {
+
+        /**
+         * Sets the unbreakable tag
+         *
+         * @param unbreakable true if set unbreakable
+         */
+        public void setUnbreakable(boolean unbreakable)
+        {
+            throw new UnsupportedOperationException( "Not supported yet." );
+        }
+
+        /**
+         * Return if the unbreakable tag is true
+         *
+         * @return true if the unbreakable tag is true
+         */
+        public boolean isUnbreakable()
+        {
+            throw new UnsupportedOperationException( "Not supported yet." );
+        }
+    }
+
+    Spigot spigot();
+    // Spigot end
+
     @SuppressWarnings("javadoc")
     ItemMeta clone();
 }
