b3e065a2b11a6dd2590606600ae58ba08e8763f8
md_5 <git@md-5.net>
2014-09-01 19:36:20 +1000

    Skip loading of plugins which are not 1.8 compatible.
    As they do not work with Spigot 1.8 builds!

--- src/main/java/org/bukkit/plugin/SimplePluginManager.java
+++ src/main/java/org/bukkit/plugin/SimplePluginManager.java
@@ -132,6 +132,18 @@ public final class SimplePluginManager implements PluginManager {
             try {
                 description = loader.getPluginDescription(file);
                 String name = description.getName();
+                // Spigot Start
+                if ( name.equalsIgnoreCase( "Orebfuscator" ) )
+                {
+                    server.getLogger().log( Level.WARNING, "Skipping loading of Orebfuscator as it does not work with Spigot 1.8 builds!" );
+                    continue;
+                }
+                if ( name.equalsIgnoreCase( "EchoPet" ) )
+                {
+                    server.getLogger().log( Level.WARNING, "Skipping loading of EchoPet as it does not work with Spigot 1.8 builds!" );
+                    continue;
+                }
+                // Spigot End
                 if (name.equalsIgnoreCase("bukkit") || name.equalsIgnoreCase("minecraft") || name.equalsIgnoreCase("mojang")) {
                     server.getLogger().log(Level.SEVERE, "Could not load '" + file.getPath() + "' in folder '" + directory.getPath() + "': Restricted Name");
                     continue;
